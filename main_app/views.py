0243c957f7cceed5eabb635ebf1ea5e84be41407
wilsonckm <wilsonckmchan@gmail.com>
2023-07-06 17:44:29 -0400

    Commit 3: Index added with respective template html

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+
+finches = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
+
 # Create your views here.
 
 def home(request):
@@ -7,3 +13,10 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
+
+
