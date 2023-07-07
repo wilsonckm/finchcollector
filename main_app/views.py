16cd51765f9cf8a5e4726380e69ed0135c961961
wilsonckm <wilsonckmchan@gmail.com>
2023-07-06 20:14:59 -0400

    Commit 4: Update, Delete, and create functionality added. NoReverseMatch bug fixed.

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-
-
-finches = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Finch
 
 # Create your views here.
 
@@ -15,8 +11,27 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
         'finches': finches
     })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {
+        'finch': finch 
+    })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url='/finches'
 
 
